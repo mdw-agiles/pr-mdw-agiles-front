02565ba7d6081f6936558c844ea791965ceb25b7
robertome <roberto.martin.escribano@alumnos.upm.es>
2019-05-06 23:48:58 +0200

    issue#3 Added real call to reservation API issue#3 Hidden summaryReservationComponent once reservation is done and show booking code

--- src/app/home/shared/models/api-endpoint.model.ts
+++ src/app/home/shared/models/api-endpoint.model.ts
@@ -2,6 +2,7 @@ export class ApiEndpoint {
   public static USERS = '/users';
   public static RESERVATION = '/reservation';
   public static RESERVATION_SEARCH = ApiEndpoint.RESERVATION + '/search';
+  public static RESERVATION_RESERVATION = ApiEndpoint.RESERVATION + '/reservation';
   public static HOTEL_CHAINS = '/chains';
   public static HOTEL = '/hotels';
   public static HOTEL_SEARCH = ApiEndpoint.HOTEL + '/search';
